3c8ed315b83009d7317b7a9ab0ad4dbc12582f0d
cristopher <christoph.alano@gmail.com>
2022-02-02 22:20:29 -0600

    reorganized the infrastructure

--- front-end/infrastructure/s3.tf
+++ front-end/infrastructure/s3.tf
@@ -1,24 +1,23 @@
-resource "aws_s3_bucket" "website_bucket" {
-    bucket = var.domain_name
-    acl = "public-read"
-    policy = data.aws_iam_policy_document.website_policy.json
-    website {
-        index_document = "index.html"
-        error_document = "404.html"
-    }
-}
+resource "aws_s3_bucket" "www" {
+  bucket = var.bucket_domain_name
+  acl    = "public-read"
 
-data "aws_iam_policy_document" "website_policy" {
-    statement {
-        actions = [
-            "s3:GetObject"
-        ]
-        principals {
-            identifiers = ["*"]
-            type = "AWS"
-        }
-        resources = [
-            "arn:aws:s3:::${var.domain_name}/*"
-        ]
+  policy = <<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.bucket_domain_name}/*"]
     }
+  ]
+}
+POLICY
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
 }
