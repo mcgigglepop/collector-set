87a260637b70e5c43a1d3c53337b8f888aaa69c4
cristopher <christoph.alano@gmail.com>
2022-02-02 22:01:22 -0600

    updated the config file

---
+++ front-end/infrastructure/config.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_file = "~/.aws/credentials"
+    profile = "AWS_PROFILE"    
+}
+terraform {
+    backend "s3" {
+        profile = "AWS_PROFILE"
+        bucket  = "REMOTE_STATE_BUCKET"
+        key     = "REMOTE_STATE_KEY.tfstate"
+        region  = "us-east-1"
+    }
+}
+
+data "aws_region" "current" {}
