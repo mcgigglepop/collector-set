b254f15c64c7cec58cbd8091a01cac6c1d93aef4
cristopher <christoph.alano@gmail.com>
2022-02-02 22:27:34 -0600

    added acm certificate resource with dns validation

---
+++ front-end/infrastructure/acm.tf
@@ -0,0 +1,5 @@
+resource "aws_acm_certificate" "acm_certificate" {
+  domain_name       = "*.${var.root_domain_name}"
+  validation_method = "DNS"
+  subject_alternative_names = ["${var.root_domain_name}"]
+}
