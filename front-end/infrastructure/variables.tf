f1cf014b5d2790c7e84043a41e6e48586a68bdad
cristopher <christoph.alano@gmail.com>
2022-02-02 22:06:50 -0600

    added a variables file

---
+++ front-end/infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "service" {
+    description = "Application service name"
+}
+
+variable "environment" {
+    description = "Application deployment environment"
+}
+
+variable "domain_name" {
+    description = "domain name for the static website"
+}
