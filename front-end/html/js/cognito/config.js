1d0e8aca74022a99e1174f0ab5106ae45c75cea5
cristopher <christoph.alano@gmail.com>
2022-02-03 19:26:25 -0600

    added config file

---
+++ front-end/html/js/cognito/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-XXX',
+        userPoolClientId: 'XXX',
+        region: 'us-east-1' 
+    },
+    api: {
+        invokeUrl: 'XXX',
+        xapikey: 'XXX'  
+    }
+};
