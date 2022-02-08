d3e67565a63e354fe5ad24bf6430c30a7d644c7e
cristopher <christoph.alano@gmail.com>
2022-02-07 23:49:23 -0600

    added the route53 zone data resource

--- front-end/infrastructure/route53.tf
+++ front-end/infrastructure/route53.tf
@@ -1,16 +1,4 @@
 data "aws_route53_zone" "zone" {
-  name                      = var.root_domain_name
-  private_zone              = false
-}
-
-resource "aws_route53_record" "route53_a_record" {
-  zone_id                   = data.aws_route53_zone.zone.zone_id
-  name                      = var.bucket_domain_name
-  type                      = "A"
-
-  alias = {
-    name                    = aws_cloudfront_distribution.cloudfront_distribution.domain_name
-    zone_id                 = aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id
-    evaluate_target_health  = false
-  }
+  name         = var.root_domain_name
+  private_zone = false
 }
