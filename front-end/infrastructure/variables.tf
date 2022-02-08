0bee96eb41fc7f249ebaaffb5c46525ca0ca4faf
cristopher <christoph.alano@gmail.com>
2022-02-07 23:54:59 -0600

    comments and new variable name

--- front-end/infrastructure/variables.tf
+++ front-end/infrastructure/variables.tf
@@ -1,17 +1,16 @@
-variable "service" {
-    description = "Application service name"
-}
+# variable "service" {
+#     description = "Application service name"
+# }
 
-variable "environment" {
-    description = "Application deployment environment"
-}
+# variable "environment" {
+#     description = "Application deployment environment"
+# }
 
 variable "root_domain_name" {
     description = "naked domain for route53"
     default     = "thecollectorset.com"
 }
 
-variable "bucket_domain_name" {
-    description = "domain name for the static website bucket"
-    default     = "www.thecollectorset.com"
+variable "bucket_name" {
+    default     = "thecollectorset.com"
 }
