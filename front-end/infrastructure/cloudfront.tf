da57ee62983e4200fa0b1331379bcf9fbcc9c601
cristopher <christoph.alano@gmail.com>
2022-02-07 23:54:27 -0600

    added the cloudfront and locals

--- front-end/infrastructure/cloudfront.tf
+++ front-end/infrastructure/cloudfront.tf
@@ -1,47 +1,85 @@
-resource "aws_cloudfront_distribution" "cloudfront_distribution" {
-  origin {
-    custom_origin_config {
-      http_port              = "80"
-      https_port             = "443"
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-    }
+locals {
+  s3_origin_id = "myS3Origin"
+}
 
-    domain_name              = aws_s3_bucket.static_website_bucket.website_endpoint
-    origin_id                = var.bucket_domain_name
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.front-end.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
   }
 
-  enabled                    = true
-  default_root_object        = "index.html"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  # If there is a 404, return index.html with a HTTP 200 Response
+  custom_error_response {
+      error_caching_min_ttl = 3000
+      error_code = 404
+      response_code = 200
+      response_page_path = "/index.html"
+  }
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${var.bucket_name}.s3.amazonaws.com"
+    prefix          = "cloudfront_logs"
+  }
+
+  aliases = [var.root_domain_name]
 
   default_cache_behavior {
-    viewer_protocol_policy   = "redirect-to-https"
-    compress                 = true
-    allowed_methods          = ["GET", "HEAD"]
-    cached_methods           = ["GET", "HEAD"]
-    target_origin_id         = var.bucket_domain_name
-    min_ttl                  = 0
-    default_ttl              = 86400
-    max_ttl                  = 31536000
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
-      query_string           = false
+      query_string = false
+
       cookies {
-        forward              = "none"
+        forward = "none"
       }
     }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
-  aliases                    = ["${var.bucket_domain_name}"]
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
   restrictions {
     geo_restriction {
-      restriction_type       = "none"
+      restriction_type = "whitelist"
+      locations        = ["US", "CA", "GB", "DE"]
     }
   }
 
   viewer_certificate {
-    acm_certificate_arn       = aws_acm_certificate.acm_certificate.arn
-    ssl_support_method        = "sni-only"
+    acm_certificate_arn = aws_acm_certificate.acm_cert.arn
+    ssl_support_method = "sni-only"
   }
+
 }
