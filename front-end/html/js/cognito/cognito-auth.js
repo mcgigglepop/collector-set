aa5d139dba14f771193be4ac5193de524cb23040
cristopher <christoph.alano@gmail.com>
2022-02-08 17:51:40 -0600

    initialize the user pool variable

--- front-end/html/js/cognito/cognito-auth.js
+++ front-end/html/js/cognito/cognito-auth.js
@@ -1,11 +1,15 @@
 var CognitoLogin = window.CognitoLogin || {};
 
 (function scopeWrapper($) {
+    // globals
+    var userPool;
 
     // cognito user pool data
     var poolData = {
         UserPoolId: _config.cognito.userPoolId,
         ClientId: _config.cognito.userPoolClientId
     };
+
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
 }(jQuery));
