e67d12307379802a9ddf903b519bfb9441484e32
cristopher <christoph.alano@gmail.com>
2022-02-08 17:50:40 -0600

    added the cognito user pool data

--- front-end/html/js/cognito/cognito-auth.js
+++ front-end/html/js/cognito/cognito-auth.js
@@ -2,4 +2,10 @@ var CognitoLogin = window.CognitoLogin || {};
 
 (function scopeWrapper($) {
 
+    // cognito user pool data
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+
 }(jQuery));
